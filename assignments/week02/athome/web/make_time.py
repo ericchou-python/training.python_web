0b1a8448fd2a2c0b52383cccbc69e836abf85bd4
Eric Chou <echou@Erics-MacBook-Air.local>
2013-01-19 09:58:35 -0800

    week 02 at home assignments

---
+++ assignments/week02/athome/web/make_time.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+make_time.py
+
+simple script that returns and HTML page with the current time
+"""
+
+import datetime
+
+time_str = datetime.datetime.now().isoformat()
+
+html = """
+<html>
+<body>
+<h2> The time is: </h2>
+<p> %s <p>
+</body>
+</html>
+"""% time_str
+
+print html
+
+
+
